8d22855cb6fc9f721042e280fff0935dcf30c241
adrish2012 <132808369+adrish2012@users.noreply.github.com>
2023-08-31 22:44:30 +0530

    Add files via upload

---
+++ CRACK-IT.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd C:\Program Files (x86)\Common Files\Adobe\OOBE\PDApp\core\
+ren PDapp.exe PDapp.exe.bak
+
+cd /d %~dp0
+
+
+copy PDapp.exe C:\Program Files (x86)\Common Files\Adobe\OOBE\PDApp\core
+
+pause
---
+++ Revert (for non working users only).bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Program Files (x86)\Common Files\Adobe\OOBE\PDApp\core\
+ren PDapp.exe.bak PDapp.exe
+exit
